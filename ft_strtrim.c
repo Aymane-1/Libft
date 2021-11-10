4b46e564b79d871b74a2521573b615df617038b9
Aeymne Echafii <aechafii@e2r1p1.1337.ma>
2021-11-10 21:22:10 +0100

    Adding the string trimming function

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,7 +6,7 @@
 /*   By: aechafii <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/09 13:22:33 by aechafii          #+#    #+#             */
-/*   Updated: 2021/11/09 21:08:47 by aechafii         ###   ########.fr       */
+/*   Updated: 2021/11/10 21:20:33 by aechafii         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include <stdio.h>
@@ -14,8 +14,8 @@
 int ft_strlen(const char *str)
 {
 	int count;
-	count = 0;
 
+	count = 0;
 	while(str[count])
 	{
 		count++;
@@ -23,39 +23,50 @@ int ft_strlen(const char *str)
 	return count;
 }
 
+int ft_is_within_s1(const char *str, char ch)
+{
+	size_t i;
+
+	i = 0;
+	while(str[i])
+	{
+		if(str[i] == ch)
+			return(1);
+		i++;
+	}
+	return(0);
+}
+
 char *ft_strtrim(char const *s1, char const *set)
 {
 	size_t i;
-	size_t j;
-	size_t len;
-	int *start;
-	int *end;
+	int length;
+	int start;
+	int end;
 	char *ptr;
 
-	start = ft_strlen((char *)s1 - 1);
-	end = ft_strlen((char *)s1 + 1);
-	ptr = (char *)malloc(sizeof(len + 1));
+	i = 0;
+	start = i;
+	while(s1[start] && ft_is_within_s1(set, s1[start]) == 1)
+		start++;
+	end = ft_strlen(s1) - 1;
+	while(s1[end] && ft_is_within_s1(set, s1[end]) == 1)
+		end --;
+	length = (end - start) + 1;
+	if (start > end)
+		length = 1;
+	ptr = (char *)malloc(length * sizeof(char));
 	if(!ptr)
 		return(ptr);
-	while(s1[i] && set[j]) 
-	{
-		if(ptr[i] != start[i])
-		ptr[i] = 0;
-		i++;
-	}
-	while(s1[i] && set[j])
-	{
-		if(ptr[i] != set[j])
-		ptr[i] = 0;
-		j--;
-	}
-	ptr[i - j] = '\0';
+	while(start <= end)
+		ptr[i++] = s1[start++];
+	ptr[i] = '\0';
 	return(ptr);
 }
 
 int main()
 {
-	char str [] = "..//#.hello#..#./";
-	char set [] = "./#";
-	printf("%s\n", ft_strtrim(str, set));
+	const char s1 [] = "jdhfd";
+	const char set [] = "./#";
+	printf("%s\n", ft_strtrim(s1, set));
 }
