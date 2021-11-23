0b9ef218d30e02b32986d4857d85bcb3f3b0da2d
Aymane Echafii <aymane@MacBook-Pro-de-Aymane.local>
2021-11-23 15:56:29 +0100

    new functions

---
+++ ft_itoa.c
@@ -0,0 +1,44 @@
+static int	nb_digits(int n)
+{
+	int		i;
+
+	i = 0;
+	if (n == 0)
+		return (1);
+	if (n < 0)
+		i = 1;
+	while (n != 0)
+	{
+		n /= 10;
+		i++;
+	}
+	return (i);
+}
+
+char	*ft_itoa(int n)
+{
+	int		size;
+	int		len;
+	long	nb;
+	char	*p;
+
+	size = 0;
+	if (n < 0)
+		size = 1;
+	len = nb_digits(n);
+	p = (char *)malloc((len + 1) * sizeof(char));
+	if (!p)
+		return (p);
+	nb = n;
+	if (nb < 0)
+		p[0] = '-';
+	if (nb < 0)
+		nb *= -1;
+	p[len] = '\0';
+	while (--len >= size)
+	{
+		p[len] = (nb % 10) + 48;
+		nb /= 10;
+	}
+	return (p);
+}
--- ft_putnbr_fd.c
+++ ft_putnbr_fd.c
@@ -10,7 +10,22 @@
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
-void ft_putnbr_fd(int n, int fd)
+
+void	ft_putnbr_fd(int n, int fd)
 {
-	write(fd, &n, 1);
+	int	nb;
+
+	nb = n;
+	if (nb < 0)
+	{
+		ft_putchar_fd('-', fd);
+		nb *= -1;
+	}
+	if (nb > 9)
+	{
+		ft_putnbr_fd(nb / 10, fd);
+		ft_putnbr_fd(nb % 10, fd);
+	}
+	else
+		ft_putchar_fd(nb + 48, fd);
 }
